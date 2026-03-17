a865e0a64edb2452eb0576491f3dc79833d6e670
Roman <Gushlenko63@mail.ru>
2026-03-18 02:56:23 +0400

    Feature: Complete encapsulation homework

---
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -0,0 +1,50 @@
+package org.skypro.skyshop.basket;
+
+import org.skypro.skyshop.product.Product;
+
+public class ProductBasket {
+    private final Product[] products = new Product[5];
+    private int size = 0;
+
+    public void addProduct(Product product) {
+        if (size < 5) {
+            this.products[size] = product;
+            size++;
+        } else System.out.println("Невозможно добавить продукт");
+    }
+
+    public int getTotalCost() {
+        int total = 0;
+        for (Product product : products) {
+            if (product != null) total += product.getPrice();
+        }
+        return total;
+    }
+
+    public void printBasket() {
+        if (size == 0) {
+            System.out.println("В корзине пусто");
+        } else {
+            for (Product product : products) {
+                if (product != null) System.out.println(product.getName() + ": " + product.getPrice());
+            }
+            System.out.println("Итого: " + getTotalCost());
+        }
+    }
+
+    public boolean containsProduct(String name) {
+        for (Product product : products) {
+            if (product != null && name.equals(product.getName())) return true;
+        }
+        return false;
+    }
+
+    public void clear() {
+        for (int i = 0; i < 5; i++) {
+            if (products[i] != null) {
+                products[i] = null;
+            }
+        }
+        size = 0;
+    }
+}
